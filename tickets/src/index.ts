99d4abb868c499c11138ad063602757fa00b5475
Scott W <scottaza@gmail.com>
2022-10-21 17:28:44 -0700

    Add start up message

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,6 +5,9 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener'
 
 const start = async () => {
+
+    console.log('Starting up!!!!')
+
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY environment variable is not defined.')
     }
