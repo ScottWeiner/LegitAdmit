0d0662550332d234a3e058500a145e6de7d03f12
Scott W <scottaza@gmail.com>
2022-10-23 16:29:37 -0700

    Fixed test parameters to account for updated model Fixed the inputs to the automated test to account for changes to the user model

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -1,11 +1,13 @@
 import request from "supertest";
 import { app } from "../../app";
 
+
+
 it('return 400 when an nonexisten user attempts sign in', async () => {
     await request(app)
         .post('/api/users/signin')
         .send({
-            email: 'test@test.com',
+            email: 'testfail@test.com',
             password: 'passwordTest123'
         })
         .expect(400)
@@ -30,7 +32,7 @@ it('resonds with a cookie when given valid creds', async () => {
         .post('/api/users/signin')
         .send({
             email: "test@test.com",
-            password: "password"
+            password: "passwordTest123"
         })
         .expect(200)
 
--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,23 +1,31 @@
 import request from "supertest";
 import { app } from "../../app";
 
+const userSignupObject = {
+
+    email: 'test@test.com',
+    password: 'passwordTest123',
+    firstName: 'Hugh',
+    lastName: 'Jass',
+    address1: '42 Evergreen Terrace',
+    address2: '',
+    city: 'Springfield',
+    state: 'IL',
+    zip: '55555'
+
+}
+
 it('returns a 201 on successful signup', async () => {
     return request(app)
         .post('/api/users/signup')
-        .send({
-            email: "test@test.com",
-            password: 'passwordTest123'
-        })
+        .send(userSignupObject)
         .expect(201)
 })
 
 it('returns a 400 for a failed sign in with invalid email ', async () => {
     return request(app)
         .post('/api/users/signup')
-        .send({
-            email: "test@testcom",
-            password: 'passwordTest123'
-        })
+        .send({ ...userSignupObject, email: 'but.com' })
         .expect(400)
 })
 
@@ -25,8 +33,7 @@ it('returns a 400 for a failed sign in with invalid password ', async () => {
     return request(app)
         .post('/api/users/signup')
         .send({
-            email: "test@test.com",
-            password: 'p'
+            ...userSignupObject, password: '123'
         })
         .expect(400)
 })
@@ -55,28 +62,19 @@ it('returns a 400 for a reuest with invalid email or password ', async () => {
 it('disallows dupliocate emails at signup', async () => {
     await request(app)
         .post('/api/users/signup')
-        .send({
-            email: "test@test.com",
-            password: 'passwordTest123'
-        })
+        .send(userSignupObject)
         .expect(201)
 
     return request(app)
         .post('/api/users/signup')
-        .send({
-            email: "test@test.com",
-            password: 'passwordTest123'
-        })
+        .send(userSignupObject)
         .expect(400)
 })
 
 it('sets a cookie after successful signup', async () => {
     const response = await request(app)
         .post('/api/users/signup')
-        .send({
-            email: "test@test.com",
-            password: 'passwordTest123'
-        })
+        .send(userSignupObject)
         .expect(201)
 
     expect(response.get('Set-Cookie')).toBeDefined();
